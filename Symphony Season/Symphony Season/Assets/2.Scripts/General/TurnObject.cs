c198c6ea6703e5ff46bf30574d60d1a8ae470935
Michael van der Sanden <michael.mvdsanden@gmail.com>
2026-01-07 14:47:20 +0100

    TurnObjects scripts

---
+++ Symphony Season/Symphony Season/Assets/2.Scripts/General/TurnObject.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class TurnObject : MonoBehaviour
+{
+    //[Header("-------------- Required Objects")]
+    [Header("-------------- Changeble Values")]
+    [SerializeField] private float turnSpeed = 1.0f;
+    [SerializeField] private Quaternion toAngle, halfwayToAngle, halfwayFromAngle;
+
+    [Header("-------------- Background Values (do not change)")]
+    public bool isRotating = false;
+    [SerializeField] private Quaternion currentAngle, startAngle, targetAngle;
+
+    private void Awake()
+    {
+        startAngle = transform.rotation;
+    }
+
+    private void Update()
+    {
+        if(isRotating) 
+        { 
+            currentAngle = transform.rotation;
+
+            var step = turnSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.RotateTowards(currentAngle, targetAngle, step);
+
+            if(currentAngle == halfwayToAngle) { targetAngle = toAngle; }
+            if(currentAngle == halfwayFromAngle) { targetAngle = startAngle; }
+            if((targetAngle == startAngle || targetAngle == toAngle) && currentAngle == targetAngle) 
+            { 
+                isRotating= false;
+            }
+        }
+    }
+
+    public void StartRotation()
+    {
+        if (!isRotating)
+        {
+            currentAngle = transform.rotation;
+            if (currentAngle == startAngle)
+            { 
+                targetAngle = halfwayToAngle; 
+            }
+            else
+            { 
+                targetAngle = halfwayFromAngle; 
+            }
+
+            isRotating = true;
+        }
+    }
+}
