0082001ded0ecd3b192226dde93a9bf6dea8fcce
Luuk <luukjacobsluuk@gmail.com>
2026-01-14 16:32:09 +0100

    Alles Eindelijk Af T0T

--- Symphony Season/Symphony Season/Assets/2.Scripts/BlockPuzzle/VictoryTrigger.cs
+++ Symphony Season/Symphony Season/Assets/2.Scripts/BlockPuzzle/VictoryTrigger.cs
@@ -4,13 +4,16 @@ public class VictoryTrigger : MonoBehaviour
 {
     public UIToggles uiToggles;
     public TriggerSetter CurtainCloser;
-
+    public MoveObject PlayerMover;
+    public GameObject PlayerMat;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             uiToggles.Victory();
             CurtainCloser.SetTrigger();
+            PlayerMover.StartMoving(true);
+            PlayerMat.GetComponent<MeshRenderer>().material.SetFloat("_IsMoving", 1f);
         }
 
     }
