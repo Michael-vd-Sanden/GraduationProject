0cc9431a32b4224509d7fdeec529621604572862
Luuk <luukjacobsluuk@gmail.com>
2026-01-14 14:15:26 +0100

    Curtains werken in de Game Zelf

--- Symphony Season/Symphony Season/Assets/2.Scripts/BlockPuzzle/VictoryTrigger.cs
+++ Symphony Season/Symphony Season/Assets/2.Scripts/BlockPuzzle/VictoryTrigger.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class VictoryTrigger : MonoBehaviour
 {
     public UIToggles uiToggles;
+    public TriggerSetter CurtainCloser;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             uiToggles.Victory();
+            CurtainCloser.SetTrigger();
         }
+
     }
 }
