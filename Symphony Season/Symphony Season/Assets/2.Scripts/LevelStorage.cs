08ac6ce51f571cfc5912a8bb1913fdbf0550211a
Luuk <luukjacobsluuk@gmail.com>
2026-01-14 15:17:56 +0100

    Everything works now

--- Symphony Season/Symphony Season/Assets/2.Scripts/LevelStorage.cs
+++ Symphony Season/Symphony Season/Assets/2.Scripts/LevelStorage.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,21 +8,34 @@ public class LevelStorage : MonoBehaviour
     public string[] Levels;
     public string[] HardModeLevels;
     public LevelIndex LevelIndex;
-    
+    public TriggerSetter CurtainCloser;
+
+    private bool hasStarted;
+
     public void HardModeShift()
     {
         if (!HardMode) { HardMode = true; }
         else if (HardMode) { HardMode = false; }
     }
 
-    public void NextScene()
+    public void StartNextScene()
     {
+        if (!hasStarted) { StartCoroutine(NextScene()); }
+    }
+
+    private IEnumerator NextScene()
+    {
+        hasStarted = true;
+        CurtainCloser.SetTrigger();
+        yield return new WaitForSecondsRealtime(2.2f);
         if (!HardMode)
         {
             SceneManager.LoadScene(Levels[LevelIndex.FloorIndex]);
-        } else if (HardMode)
+        }
+        else if (HardMode)
         {
             SceneManager.LoadScene(HardModeLevels[LevelIndex.FloorIndex]);
         }
+        hasStarted = false;
     }
 }
