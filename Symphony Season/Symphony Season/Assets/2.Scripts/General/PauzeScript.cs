692df28285708f03b6e5a1224c5a9ce8d1dcef16
Michael van der Sanden <michael.mvdsanden@gmail.com>
2026-01-23 13:03:50 +0100

    Pauze menu af

--- Symphony Season/Symphony Season/Assets/2.Scripts/General/PauzeScript.cs
+++ Symphony Season/Symphony Season/Assets/2.Scripts/General/PauzeScript.cs
@@ -3,20 +3,24 @@ using UnityEngine;
 public class PauzeScript : MonoBehaviour
 {
     private SceneSwitching sceneSwitch;
+    private TriggerSetter curtainTriggers;
 
     private void Awake()
     {
         sceneSwitch = FindFirstObjectByType<SceneSwitching>();
+        curtainTriggers = FindFirstObjectByType<TriggerSetter>();
     }
 
     public void Pauze()
     {
-        Time.timeScale = 0f;
+        curtainTriggers.SetTrigger();
+        //Time.timeScale = 0f;
     }
 
     public void UnPauze()
     {
-        Time.timeScale = 1.0f;
+        //Time.timeScale = 1.0f;
+        curtainTriggers.SetTrigger();
     }
 
     public void MainMenu()
