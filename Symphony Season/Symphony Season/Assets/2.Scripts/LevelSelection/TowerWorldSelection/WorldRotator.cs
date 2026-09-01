09cf8bb0b80714dd409412d1a05712020327dcb7
recurracy3 <renetjuhhh14@gmail.com>
2026-09-01 12:00:43 +0200

    Rotate the boat with the angular velocity of the planet.

--- Symphony Season/Symphony Season/Assets/2.Scripts/LevelSelection/TowerWorldSelection/WorldRotator.cs
+++ Symphony Season/Symphony Season/Assets/2.Scripts/LevelSelection/TowerWorldSelection/WorldRotator.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework.Constraints;
 using UnityEditor;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class WorldRotator : MonoBehaviour
 {
     [Header("-------------- Required Objects")]
     [SerializeField] private InputActionReference deltaAction;
+    [SerializeField] private GameObject boat;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,18 +20,31 @@ public class WorldRotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var rb = gameObject.GetComponent<Rigidbody>();
+        Debug.Log("Ang vel " + rb.angularVelocity.ToString());
+        var dir = rb.angularVelocity.normalized;
+        float ang = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        var boatRotation = Quaternion.Lerp(boat.transform.rotation, Quaternion.Euler(0, (ang*1)-0, 0), Time.deltaTime * 5);
+        boat.transform.rotation = boatRotation;
+    }
+
+    private void RotateGlobe(Vector2 dir, float speed)
+    {
+        var rb = gameObject.GetComponent<Rigidbody>();
+        var norm = dir.normalized;
+        rb.angularVelocity += new Vector3(norm.y, norm.x, 0) * speed;
+        float ang = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
     }
 
     private void Move(InputAction.CallbackContext obj)
     {
         float sensitivity = 1.0f;
-        var rb = gameObject.GetComponent<Rigidbody>();
-        var delta = obj.ReadValue<Vector2>() * sensitivity * Time.deltaTime;
+        Vector2 delta = obj.ReadValue<Vector2>() * sensitivity * Time.deltaTime;
         delta.x = Mathf.Clamp(delta.x, -1, 1);
         delta.y = Mathf.Clamp(delta.y, -1, 1);
-        rb.angularVelocity += new Vector3(delta.y, delta.x, 0) * delta.magnitude;
-        // rb.angularVelocity += new Vector3(delta.x, delta.y, 0);
+        Vector2 dir = delta.normalized;
+        float speed = delta.magnitude;
+        RotateGlobe(obj.ReadValue<Vector2>(), speed);
     }
 
     private void OnEnable()
