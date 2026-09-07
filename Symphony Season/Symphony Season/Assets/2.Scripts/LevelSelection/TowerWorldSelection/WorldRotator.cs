97631eb4e5f6eb3c7d7a057716aba201446ba59e
recurracy3 <renetjuhhh14@gmail.com>
2026-09-07 15:27:16 +0200

    Empty check so the boat doesn't rotate all the time.

--- Symphony Season/Symphony Season/Assets/2.Scripts/LevelSelection/TowerWorldSelection/WorldRotator.cs
+++ Symphony Season/Symphony Season/Assets/2.Scripts/LevelSelection/TowerWorldSelection/WorldRotator.cs
@@ -21,6 +21,10 @@ public class WorldRotator : MonoBehaviour
     void Update()
     {
         var rb = gameObject.GetComponent<Rigidbody>();
+        if (rb.angularVelocity == Vector3.zero)
+        {
+            return;
+        }
         var dir = rb.angularVelocity.normalized;
         float ang = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         var boatRotation = Quaternion.Lerp(boat.transform.rotation, Quaternion.Euler(0, (ang*1)-0, 0), Time.deltaTime * 5);
