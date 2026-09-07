e054c0202c973c6e4e1e8424c1c933d478b18c2a
recurracy3 <renetjuhhh14@gmail.com>
2026-09-07 15:46:08 +0200

    Update JSONHandler to prepare saving score information

--- Symphony Season/Symphony Season/Assets/2.Scripts/JSON/JSONHandler.cs
+++ Symphony Season/Symphony Season/Assets/2.Scripts/JSON/JSONHandler.cs
@@ -4,14 +4,42 @@ using Newtonsoft.Json;
 using System.Diagnostics.Contracts;
 using System.Diagnostics;
 using System.IO;
+using System;
+using UnityEditor.UI;
+using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using Unity.VisualScripting;
+using System.Linq;
 
 public class SSeasonJSONData
 {
     public int LevelIndex { get; set; }
+    /// <summary>
+    /// This is a dictionary so we can store scores per level.
+    /// Key: Level name.
+    /// Value: SSeasonJSONLevelData instance.
+    /// </summary>
+    public Dictionary<string, SSeasonJSONLevelData> ScoresPerLevel { get; set; }
+}
+
+public class SSeasonJSONLevelData
+{
+    public SSeasonJSONScoreData HighScore { get; set; }
+    // List of scores for this level, max of 5.
+    public List<SSeasonJSONScoreData> Scores { get; set; }
+    // Gets updated every time the player stores their last score. Loops back to 0 if > 5.
+    public int LastSavedScoreIndex { get; set; }
+}
+
+public class SSeasonJSONScoreData
+{
+    public DateTime Date { get; set; }
+    public int Score { get; set; }
 }
 
 public class JSONHandler : MonoBehaviour
 {
+    public SSeasonJSONData JsonData { get; set; }
     public static JSONHandler Instance { get; private set; }
 
     void Awake()
@@ -40,7 +68,7 @@ public class JSONHandler : MonoBehaviour
     public SSeasonJSONData GetJSONData()
     {
         string path = Application.persistentDataPath + "/sseason.json";
-        SSeasonJSONData retData = new SSeasonJSONData();
+        JsonData = new SSeasonJSONData();
 
         if (File.Exists(path))
         {
@@ -50,10 +78,25 @@ public class JSONHandler : MonoBehaviour
                 output = sr.ReadToEnd();
             }
 
-            retData = JsonConvert.DeserializeObject<SSeasonJSONData>(output);
+            JsonData = JsonConvert.DeserializeObject<SSeasonJSONData>(output);
         }
 
-        return retData;
+        return JsonData;
+    }
+
+    public void AddScore(string levelName, DateTime date, int score)
+    {
+        SSeasonJSONLevelData lvlScore;
+        if (!JsonData.ScoresPerLevel.TryGetValue(levelName, out lvlScore))
+        {
+            lvlScore = new SSeasonJSONLevelData();
+        }
+
+        lvlScore.LastSavedScoreIndex += 1;
+        if (lvlScore.LastSavedScoreIndex > 5)
+        {
+            lvlScore.LastSavedScoreIndex = 0;
+        }
     }
 
     public void WriteJSON(SSeasonJSONData data)
